93a2ed4527c47fb3cad9d4585d521ee6b4175914
owsei <owseiwastaken@gmail.com>
2022-11-24 10:30:12 -0300

    ADrawPartCollum

--- adraw.c
+++ adraw.c
@@ -30,4 +30,34 @@ void ADrawPartLine (
 	}
 }
 
+void ADrawPartCollum (
+	struct fbjar jar,
+	int x, int StartY, int EndY,
+	color RGB,
+	int thick
+) {
+	int skip = jar.bpp;
+	if (thick < 0) {
+		thick = -thick;
+		skip = -skip;
+	}
 
+	assert(CheckInjar(jar, StartY, x));
+	assert(CheckInjar(jar, EndY, x+thick));
+	assert(thick!=0);
+
+	char* location = GetFbPos(jar, StartY, x);
+	int Ylen = (EndY - StartY);
+	int sk, y, i;
+
+	for (i = 0 ; i<thick ; i++) {
+		sk = jar.yoff*jar.skip;
+		for (y = 0 ; y<Ylen ; y++) {
+			sk+=jar.skip;
+			*(location + 0 + sk) = RGB.B;
+			*(location + 1 + sk) = RGB.G;
+			*(location + 2 + sk) = RGB.R;
+		}
+		location += skip;
+	}
+}
