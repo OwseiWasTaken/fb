4e0b8c932a78f6eb4a5fc93f8e291f532ffb5466
owsei <owseiwastaken@gmail.com>
2022-11-25 18:44:14 -0300

    testing chars.c

--- buff.c
+++ buff.c
@@ -18,22 +18,36 @@
 // include initers / funcs
 #include "vars.c"
 
-// include draw funcs
-#include "draw.c"
-// include Advanced draw funcs
-#include "adraw.c"
+#include "draw/simple.c"
+#include "draw/normal.c"
+#include "draw/chars.c"
 
 
 int main ( void ) {
 	HideCursor();
 	struct fbjar jar = InitFb();
-	//InitTerm(jar);
+	fprintf(jar.log, "fb size: line cols:%d, rows: %d\n", jar.cols-1, jar.rows-1);
+	fprintf(jar.log, "tty: %s\n", jar.tty);
+	uint8* chars = ReadChars(1);
+	fprintf(jar.log, "chars loaded from 'draw/font'\n");
 
-	fprintf(jar.log, "fb size: line cols:%d, rows: %d\n\n", jar.cols-1, jar.rows-1);
-	fflush(jar.log);
 
+	//
+	bool* chr = ExpandMappedChar(chars, 0);
 
-	getc(stdin);
+	for (int i = 0 ; i<CHARLEN; i++) {
+		for (int j = 0 ; j<CHARLINELEN ; j++) {
+			fprintf(jar.log, "%s", *(chr+i*CHARLEN+j)?"@":".");
+		}
+		fprintf(jar.log, "\n");
+	}
+	DrawChar(jar, chr, 2, 2);
+
+	//for (int i = 0 ; i<CHARLEN ; i++) {
+	//	printf("%d:%d\n", i, *(chars+i));
+	//}
+
+	//
 
 	//CloseTerm(jar);
 	// close framebuffer jar
