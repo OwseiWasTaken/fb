1a47dd625e36cdb742d3b2b16ebf233d416c41b1
owsei <owseiwastaken@gmail.com>
2022-11-25 19:38:10 -0300

    chars working bro!

--- draw/chars.c
+++ draw/chars.c
@@ -7,7 +7,7 @@
 uint8* ReadChars (int len) {
 	int fontfd = open("./draw/font", O_RDWR);
 	uint8 *chars = malloc(CHARLEN*len);
-	chars = (unsigned char *)mmap(0,
+	chars = (uint8 *)mmap(0,
 			CHARLEN*len, PROT_READ, MAP_SHARED, fontfd, 0);
 	return chars;
 }
@@ -22,7 +22,7 @@ bool* ExpandChar(uint8* from) {
 	bool *ch = malloc(CHARLEN);
 	for (int i = 0 ; i<CHARLEN; i++) {
 		for (uint j = 0 ; j<CHARLINELEN ; j++) {
-			*(ch + i*CHARLEN + j ) = (*(from+ i) & (1<<j))^0;
+			*(ch + i*CHARLEN + j ) = (*(from+ i) & (1<<j))!=0;
 			//fprintf(jar.log, "%d:%d -> %d\n", (1<<j), *(from+ i), *(ch + i));
 		}
 	}
@@ -33,30 +33,25 @@ bool* ExpandMappedChar(uint8* from, int index) {
 	return ExpandChar((from+CHARLEN*index));
 }
 
-void DrawChar(struct fbjar jar, bool* chr, int y, int x) {
-	char* location = GetFbPos(jar, y, x);
-	bool* ch = chr;
+void DrawChar(struct fbjar jar, uint8* chr, int y, int x) {
+	uint8* location = GetFbPos(jar, y, x);
 	for (int i = 0 ; i<CHARLEN; i++) {
-		for (int j = 0 ; j<CHARLINELEN ; j++) {
-			*(location+0) = *(ch)*255;
-			*(location+1) = *(ch)*255;
-			*(location+2) = *(ch)*255;
-			fprintf(jar.log, "%s-%d:%d\n",
-			*(ch)!=0?"@":" ",
-			i+y, x+j);
+		fprintf(jar.log, "\nLine: %d\n", i);
+		for (uint j = 0 ; j<CHARLINELEN ; j++) {
+			*(location+0) = ((*(chr + i) & (1<<j))!=0)*255;
+			*(location+1) = ((*(chr + i) & (1<<j))!=0)*255;
+			*(location+2) = ((*(chr + i) & (1<<j))!=0)*255;
+			fprintf(jar.log, "%d&%d=%d->",
+				*(chr + i),(1<<j),
+				(*(chr+i)&(1<<j))!=0
+			);
+			fprintf(jar.log, "%d\n", *(location+1));
+			// next pixel
 			location += jar.bpp;
-			ch++;
 		}
+		// next line
 		location += jar.skip;
 		location -= CHARLINELEN*jar.bpp;
-		//ch += CHARLEN;
-		fprintf(jar.log, "\n");
-		//location += jar.skip;
 	}
-	//for (int i = 0 ; i<CHARLEN; i++) {
-	//	for (uint j = 0 ; j<CHARLINELEN ; j++) {
-	//		*(location + jar.bpp*i + jar.skip*j) = *(chr+i*CHARLINELEN+j);
-	//	}
-	//}
+	return;
 }
-
