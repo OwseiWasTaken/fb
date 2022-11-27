b4fd2568659ff6b2d3e2532504ff4f8057308b70
owsei <owseiwastaken@gmail.com>
2022-11-27 18:11:18 -0300

    font file into font.c (just testing)

--- draw/chars.c
+++ draw/chars.c
@@ -3,33 +3,35 @@
 #define CHARLEN 16
 #define CHARLINELEN 8
 
-// read chars from draw/font
-uint8* ReadChars (int len) {
-	int fontfd = open("./draw/font", O_RDWR);
-	uint8 *chars = malloc(CHARLEN*len);
-	chars = (uint8 *)mmap(0,
-			CHARLEN*len, PROT_READ, MAP_SHARED, fontfd, 0);
-	return chars;
-}
+#include "font.c"
 
-//for (int i = 0 ; i<CHARLEN; i++) {
-	//for (int j = 0 ; j<8 ; j++) {
-	//	fprintf(jar.log, "%s", *(ch+i*CHARLEN+j)?"@":".");
-	//}
+//// read chars from draw/font
+//uint8* ReadChars (int len) {
+//	int fontfd = open("./draw/font", O_RDWR);
+//	uint8 *chars = malloc(CHARLEN*len);
+//	chars = (uint8 *)mmap(0,
+//			CHARLEN*len, PROT_READ, MAP_SHARED, fontfd, 0);
+//	return chars;
 //}
+//
+////for (int i = 0 ; i<CHARLEN; i++) {
+//	//for (int j = 0 ; j<8 ; j++) {
+//	//	fprintf(jar.log, "%s", *(ch+i*CHARLEN+j)?"@":".");
+//	//}
+////}
 
-void DrawChar(struct fbjar jar, uint8* chr, int y, int x) {
+void DrawChar(struct fbjar jar, int y, int x) {
 	uint8* location = GetFbPos(jar, y, x);
 	for (int i = 0 ; i<CHARLEN; i++) {
 		fprintf(jar.log, "\nLine: %d\n", i);
 		for (uint j = 0 ; j<CHARLINELEN ; j++) {
-			*(location+0) = ((*(chr + i) & (1<<j))!=0)*255;
-			*(location+1) = ((*(chr + i) & (1<<j))!=0)*255;
-			*(location+2) = ((*(chr + i) & (1<<j))!=0)*255;
-			fprintf(jar.log, "%d&%d=%d->",
-				*(chr + i),(1<<j),
-				(*(chr+i)&(1<<j))!=0
-			);
+			*(location+0) = ((font[i] & (1<<j))!=0)*255;
+			*(location+1) = ((font[i] & (1<<j))!=0)*255;
+			*(location+2) = ((font[i] & (1<<j))!=0)*255;
+			//fprintf(jar.log, "%d&%d=%d->",
+			//	font[i],(1<<j),
+			//	(font[i]&(1<<j))!=0
+			//);
 			fprintf(jar.log, "%d\n", *(location+1));
 			// next pixel
 			location += jar.bpp;
