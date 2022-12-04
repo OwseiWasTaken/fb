e453902938ac8dacf23db8760033b2e577b9d84c
owsei <owseiwastaken@gmail.com>
2022-12-04 20:31:34 -0300

    testing bit & byte maps

--- examples/BitmapTest.c
+++ examples/BitmapTest.c
@@ -4,42 +4,32 @@ void ShowByteMap(bytesmap m);
 
 int main (int argc, char** argv) {
 	struct fbjar jar = InitBuffy();
-	bytesmap b;
+
+	//bitmap b;
+	//b.heigth = 2;
+	//b.width = 8;
+	//bool cont[] = {
+	//	1,0,1,0,1,0,1,0,
+	//	0,1,0,1,0,1,0,1
+	//};
+	//b.cont = cont;
+	//SDrawBitmap(jar, b, MakePoint(300,300));
+	bytemap b;
 	b.heigth = 8;
-	b.width = 1;
-	color cont[] = {
-		RGB(255,0,127),
-		RGB(255,0,127),
-		RGB(255,0,127),
-		RGB(255,0,127),
-		RGB(255,0,127),
-		RGB(255,0,127),
-		RGB(255,0,127),
-		RGB(255,0,127)
+	b.width = 3;
+	byte cont[] = {
+		255,0,127,
+		255,0,127,
+		255,0,127,
+		255,0,127,
+		255,0,127,
+		255,0,127,
+		255,0,127,
+		255,0,127
 	};
 	b.cont = cont;
 	point at = MakePoint(100,1000);
-	point end = MakePoint(at.y+b.heigth, at.x+b.width);
 	if (argc-1) {
-		DrawBytesmap(jar, b, at);
-		//SDrawBitmap(jar, b, at);
-	}
-	//printf("%ld\n", sizeof(color));
-	bytesmap back = SaveBytes(jar, at, end);
-	printf("ByteMap size: %dX%d\n", back.heigth, back.width);
-	printf("ByteMap size: %dX%d\n", b.heigth, b.width);
-	ShowByteMap(b);
-}
-
-void ShowByteMap(bytesmap m) {
-	for (int i = 0 ; i<m.heigth ; i++) {
-		for (int j = 0 ; j<m.width ; j++) {
-			printf("%.3d,%.3d,%.3d, ",
-				m.cont[i*m.width+j].R,
-				m.cont[i*m.width+j].G,
-				m.cont[i*m.width+j].B
-			);
-		}
-		putc('\n', stdout);
+		DrawBytemap(jar, b, at);
 	}
 }
