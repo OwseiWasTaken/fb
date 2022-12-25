8b523724f37d95ead1339b504087531cc84a0830
owsei <owseiwastaken@gmail.com>
2022-12-25 02:20:44 -0300

    t/bmap lil argv parse start

--- tools/bmap/main.go
+++ tools/bmap/main.go
@@ -8,7 +8,36 @@ include "bytemap.go"
 include "bytesmap.go"
 
 func main() {
-	PS(load("here.bim"))
-	PS(load("here.btm"))
-	PS(load("here.btsm"))
+	// flags
+	var (
+		interact = true
+		asf = false // already saved file
+		export = false // auto export
+		ExpId int
+		expto string
+		objname string = "new"
+	)
+
+	for i:=0; i<len(argv); i++{
+		if argv[i][0] != '-' {
+			assert(!asf, spf("no overwrite of input $%d→\"%s\"", i, argv[i]))
+			asf = true
+			objname, ExpId = GetObjAndType(argv[i])
+		} else if argv[i] == "-e" {
+			i++
+			if !(i < len(argv)) {continue}
+			expto = argv[i]
+			objname, ExpId = GetObjAndType(expto)
+			export = true
+		} else if argv[i] == "-s" { // static
+			interact = false
+		} else {
+			die(spf("can't parse arg %d \"%s\"", i, argv[i]))
+		}
+	}
+
+	PS("open at `", objname, "` i:", interact, " e:",asf, " do e:", export, " o:`",expto, "` otype:",MapToExt[ExpId])
+	//b:=load("here.bim").(bitmap)
+	//b.btcont[0] = 0xae
+	//export(b, MAP_)
 }
