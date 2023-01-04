f05210869a54e78e2114ff92fdc94d6726723194
owsei <owseiwastaken@gmail.com>
2023-01-04 20:10:47 -0300

    D2LineLerp

--- examples/line.c
+++ examples/line.c
@@ -1,26 +1,63 @@
 #include "buffy.h"
 
-int main(void) {
+// this is wrong
+// it lerps thru every line once,
+// insted of halfing the number of lines until a single point is reached
+inline ppoint DNLineLerp(line lines[], int lnc /* line count */, lfloat t) {
+	line rn = lines[0];
+	for (int i = 1; i<lnc; i++) {
+		rn = pMakeLine(
+			D1LineLerp(rn, t),
+			D1LineLerp(lines[i], t)
+		);
+	}
+	return D1LineLerp(rn, t);
+}
+
+inline ppoint D2LineLerp(point spot[], lfloat t) {
+	return D1pLineLerp(
+	MakepLine(
+		D1PointLerp(spot[0], spot[1], t),
+		D1PointLerp(spot[1], spot[2], t)
+	) , t);
+}
+
+int main(int argc, char *argv[]) {
 	struct fbjar jar = InitBuffy();
 
-	line la = MakeLine(
-		MakePoint(50,0), // top
-		MakePoint(150,0) // control
-	);
-	line lb = MakeLine(
-		MakePoint(150,0), // control
-		MakePoint(150,100) // bot
-	);
-	line bz;
+	printf("%d%s\n", argc, *argv);
+	color pink = RGB(255,128,128);
 
-	for (float i = 0.0; i<101.0;i++) {
-		bz = MakeLine(
-			GetPointByLineSeg(la, i),
-			GetPointByLineSeg(lb, i)
-		);
-		SDrawPoint( jar, GetPILSeg(bz, i) );
-		SDrawPoint( jar, GetPILSeg(la, i) );
-		SDrawPoint( jar, GetPILSeg(lb, i) );
+	point tr[] = {
+		MakePoint(100+ 000,100+ 200),
+		MakePoint(100+ 200,100+ 200),
+		MakePoint(100+ 200,100+ 400)
+	};
+	point tl[] = {
+		MakePoint(100+ 200,100+ 000),
+		MakePoint(100+ 200,100+ 200),
+		MakePoint(100+ 000,100+ 200)
+	};
+	point bl[] = {
+		MakePoint(100+ 400,100+ 200),
+		MakePoint(100+ 200,100+ 200),
+		MakePoint(100+ 200,100+ 000)
+	};
+	point br[] = {
+		MakePoint(100+ 200,100+ 400),
+		MakePoint(100+ 200,100+ 200),
+		MakePoint(100+ 400,100+ 200),
+	};
+
+	lfloat p = 600;
+
+	for (lfloat t = 0.0f; t < 1.01; t+=1/p) {
+		DrawpPoint(jar, D2LineLerp(tr, t), pink);
+		DrawpPoint(jar, D2LineLerp(tl, t), pink);
+		DrawpPoint(jar, D2LineLerp(bl, t), pink);
+		DrawpPoint(jar, D2LineLerp(br, t), pink);
+
+		usleep(8000);
 	}
 
 	StopBuffy(jar);
